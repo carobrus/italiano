6b795aa029274db8c2f32d61cad33ea46f0b308a
Carolina Bruscantini <bruscantinic@gmail.com>
2022-04-24 01:34:47 -0300

    Add navbar responsiveness

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,12 +10,12 @@ const pages = [
 const Navbar = (): JSX.Element => {
     return (
         <nav className="bg-black text-white text-lg flex justify-center">
-            <div className="flex items-center space-x-8 py-3 max-w-custom-nav w-full">
+            <div className="flex items-center space-x-6 md:space-x-8 py-1 md:py-3 max-w-custom-nav w-full">
                 <Link to={'/'}>
-                    <ItalyIcon className='ml-6 hover:opacity-80' />
+                    <ItalyIcon className='ml-6 hover:opacity-80 w-8 md:w-12' />
                 </Link>
                 {pages.map(p =>
-                    <Link to={p.url} key={p.name} className='hover:text-neutral-300'>{p.name}</Link>
+                    <Link to={p.url} key={p.name} className='hover:text-neutral-300 text-sm md:text-base'>{p.name}</Link>
                 )}
             </div>
         </nav>
